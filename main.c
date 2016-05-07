cd45f10eced5c743ba95a0efab6641feaa2f5657
Ruben <ruben2209@live.com>
2016-05-07 00:42:18 -0500

    primer proyecto

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <iostream.h>
+#include <stdlib.h>
+
+int main ()
+{	
+	int a,b,suma;
+	cout<<"introduce las dos variables:";
+	cin>>a,b;
+	suma=a+b;
+	cout<<"el resultado es:"<<suma;
+	
+	system ("pause");
+	return 0
+}
